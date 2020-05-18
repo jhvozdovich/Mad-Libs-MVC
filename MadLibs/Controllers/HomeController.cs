22a5c65e15b184c72b27f357541bd5b67bb5e425
nitunwa <nitun.2@gmail.com>
2020-05-18 14:00:33 -0700

    add Halloween form and Mad Libs with index

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ namespace MadLibs.Controllers{
     [Route("/hello")]
     public string Hello(){return "Hi World" ;}
     [Route("/")]
+    public ActionResult Index() { return View(); }
+    [Route("/general")]
     public ActionResult Form() { return View(); }
     [Route("/madlibs")]
     public ActionResult MadLibs(string noun,string adjective,string adverb){
@@ -14,6 +16,29 @@ namespace MadLibs.Controllers{
       myWordVariable.Adjective = adjective;
       myWordVariable.Adverb = adverb;
       return View(myWordVariable);
+    }
+    [Route("/formHalloween")]
+    public ActionResult HalloweenForm(){return View();}
+    [Route("/madlibsHalloween")]
+    public ActionResult Halloween(string verb1,string adjective1,
+    string adjective2,string pluralNoun1,string pluralNoun2,string noun1,
+    string adjective3,string color, string noun2, string adjective4,
+    string animal,string verb2,string verb3){
+    HalloweenVariable myHalloweenVariable = new HalloweenVariable();
+    myHalloweenVariable.Verb1 = verb1;
+    myHalloweenVariable.Adjective1 = adjective1;
+    myHalloweenVariable.Adjective2 = adjective2;
+    myHalloweenVariable.PluralNoun1 = pluralNoun1;
+    myHalloweenVariable.PluralNoun2 = pluralNoun2;
+    myHalloweenVariable.Noun1 = noun1;
+    myHalloweenVariable.Adjective3 = adjective3;
+    myHalloweenVariable.Color = color;
+    myHalloweenVariable.Noun2 = noun2;
+    myHalloweenVariable.Adjective4 = adjective4;
+    myHalloweenVariable.Animal = animal;
+    myHalloweenVariable.Verb2 = verb2;
+    myHalloweenVariable.Verb3 = verb3;
+    return View(myHalloweenVariable);
     }
   }
 }
---
+++ MadLibs/Models/HalloweenVariable.cs
@@ -0,0 +1,20 @@
+namespace MadLibs.Models
+{
+  public class HalloweenVariable
+  {
+    public string Verb1 { get; set; }
+    public string Adjective1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string PluralNoun1 { get; set; }
+    public string PluralNoun2 { get; set; }
+    public string Noun1 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Color { get; set; }
+    
+    public string Noun2 { get; set; }
+    public string Adjective4 { get; set; }
+    public string Animal { get; set; }
+    public string Verb2 { get; set; }
+    public string Verb3 { get; set; }
+  }
+}
